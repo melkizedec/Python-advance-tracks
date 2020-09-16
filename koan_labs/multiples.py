c92b8fe66a11ee90cbdea3e57b0fd7a8186356d4
Owuor <melkizedecowuor@gmail.com>
2020-09-16 16:51:43 +0300

    completed multiple activity

--- koan_labs/multiples.py
+++ koan_labs/multiples.py
@@ -1,4 +1,11 @@
 def multiple_sum():
 	#TODO: Your code goes here
-	return
+	list_number = []
+	indie = range(1,1000)
+	for x in indie:
+		if x % 3 == 0 or x % 5 == 0:
+			list_number.append(x)
+
+
+	return sum(x for x in list_number)
 	
